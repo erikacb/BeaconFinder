77135110fb689f8130c146c8800cef7ef1ff7fe5
Erika Bueno <erikacb@id.uff.br>
2016-07-28 00:01:23 -0300

    Initial Commit

---
+++ BeaconFinder/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BeaconFinder
+//
+//  Created by Erika Bueno on 28/07/16.
+//  Copyright © 2016 Erika Bueno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BeaconFinder/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BeaconFinder
+//
+//  Created by Erika Bueno on 28/07/16.
+//  Copyright © 2016 Erika Bueno. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
